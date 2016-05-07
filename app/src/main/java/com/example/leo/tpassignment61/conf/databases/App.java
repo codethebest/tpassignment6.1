d613c3cc393caf0329ed8e9b2d30924b5ac33db3
LiyoloLeo <leo.moko8@gmail.com>
2016-05-07 09:56:50 -0700

    updates almost done with

---
+++ app/src/main/java/com/example/leo/tpassignment61/conf/databases/App.java
@@ -0,0 +1,25 @@
+package com.example.leo.tpassignment61.conf.databases;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Leo on 4/25/2016.
+ */
+public class App extends Application{
+    private static Context context;
+    private static App singleton;
+    public void onCreate(){
+        super.onCreate();
+        App.context= getApplicationContext();
+        singleton=this;
+    }
+
+    public Context getContext() {
+        return App.context;
+    }
+    public static  synchronized App getInstance(){
+        return  singleton;
+    }
+}
+
